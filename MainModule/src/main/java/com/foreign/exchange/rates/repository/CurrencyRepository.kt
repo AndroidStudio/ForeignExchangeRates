72aa54396a85db7050c11fa79fa2899b9ad1af1e
Przemek <przemek33665@gmail.com>
2019-03-22 09:24:32 +0100

    update

--- MainModule/src/main/java/com/foreign/exchange/rates/repository/CurrencyRepository.kt
+++ MainModule/src/main/java/com/foreign/exchange/rates/repository/CurrencyRepository.kt
@@ -9,6 +9,7 @@ import javax.inject.Inject
 class CurrencyRepository @Inject constructor(private val sharedPreferences: SharedPreferences) {
 
     fun setCurrency(currency: String) {
+
         sharedPreferences.edit { putString(BASE_CURRENCY, currency) }
         Timber.d("CurrencyRepository setCurrency %s", currency)
     }
