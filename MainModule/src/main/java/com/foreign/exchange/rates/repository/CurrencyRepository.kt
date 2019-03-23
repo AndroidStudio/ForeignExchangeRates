1b408f38935d990f0b55b3d972202ebe5ad18ee7
Przemek <przemek33665@gmail.com>
2019-03-23 20:57:08 +0100

    Revert "update dagger"
    This reverts commit 182f8ec

--- MainModule/src/main/java/com/foreign/exchange/rates/repository/CurrencyRepository.kt
+++ MainModule/src/main/java/com/foreign/exchange/rates/repository/CurrencyRepository.kt
@@ -9,14 +9,11 @@ import javax.inject.Inject
 class CurrencyRepository @Inject constructor(private val sharedPreferences: SharedPreferences) {
 
     fun setCurrency(currency: String) {
-
         sharedPreferences.edit { putString(BASE_CURRENCY, currency) }
         Timber.d("CurrencyRepository setCurrency %s", currency)
     }
 
     fun getCurrency(): String {
-
-
         return sharedPreferences.getString(BASE_CURRENCY, "EUR") ?: "EUR"
     }
 }
